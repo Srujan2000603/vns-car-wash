c7d63105788bb29567ffd2466a5fe36e2af38cc0
Srujan2000603 <srujanmothe407@gmail.com>
2025-08-21 15:03:48 +0400

    Update share.js

--- JS/share.js
+++ JS/share.js
@@ -11,25 +11,25 @@ function createShareModal() {
           <span class="close-modal" onclick="document.getElementById('shareModal').remove()">×</span>
           <h3>Share This Card</h3>
           <div class="share-buttons">
-            <a class="modal-share-button whatsapp-share" href="https://wa.me/?text=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on WhatsApp">
+            <a class="modal-share-button whatsapp-share" href="https://wa.me/?text=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on WhatsApp">
               <i class="fab fa-whatsapp"></i>
             </a>
-            <a class="modal-share-button facebook-share" href="https://www.facebook.com/sharer/sharer.php?u=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on Facebook">
+            <a class="modal-share-button facebook-share" href="https://www.facebook.com/sharer/sharer.php?u=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on Facebook">
               <i class="fab fa-facebook"></i>
             </a>
-            <a class="modal-share-button twitter-share" href="https://twitter.com/intent/tweet?url=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on Twitter">
+            <a class="modal-share-button twitter-share" href="https://twitter.com/intent/tweet?url=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on Twitter">
               <i class="fab fa-twitter"></i>
             </a>
-            <a class="modal-share-button email-share" href="mailto:?subject=Check%20out%20this%20business%20card&body=https://diplomat-properties.github.io/Adnan-Albakkour/" title="Share via Email">
+            <a class="modal-share-button email-share" href="mailto:?subject=Check%20out%20this%20business%20card&body=https://srujan2000603.github.io/vns-car-wash/" title="Share via Email">
               <i class="fas fa-envelope"></i>
             </a>
-            <a class="modal-share-button telegram-share" href="https://t.me/share/url?url=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on Telegram">
+            <a class="modal-share-button telegram-share" href="https://t.me/share/url?url=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on Telegram">
               <i class="fab fa-telegram"></i>
             </a>
-            <a class="modal-share-button linkedin-share" href="https://www.linkedin.com/sharing/share-offsite/?url=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on LinkedIn">
+            <a class="modal-share-button linkedin-share" href="https://www.linkedin.com/sharing/share-offsite/?url=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on LinkedIn">
               <i class="fab fa-linkedin-in"></i>
             </a>
-            <a class="modal-share-button messenger-share" href="fb-messenger://share/?link=https://diplomat-properties.github.io/Adnan-Albakkour/" target="_blank" title="Share on Messenger">
+            <a class="modal-share-button messenger-share" href="fb-messenger://share/?link=https://srujan2000603.github.io/vns-car-wash/" target="_blank" title="Share on Messenger">
               <i class="fab fa-facebook-messenger"></i>
             </a>
           </div>
@@ -45,3 +45,4 @@ function createShareModal() {
 
   // Attach to Share Card button
   document.querySelector('.share-card-button').addEventListener('click', createShareModal);
+
